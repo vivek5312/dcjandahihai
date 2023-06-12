af1b29c12f1e5b545bf6eacc40d115b0d1f9cff0
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-06-12 16:51:45 +0530

    final

---
+++ dom.js
@@ -0,0 +1,7 @@
+console.log(document.title);
+
+let header=document.getElementById("header-title");
+header.style.borderBottom='3px solid black';
+let add=(document.getElementById("me"));
+add.style.color="green";
+add.style.fontWeight="bold";
